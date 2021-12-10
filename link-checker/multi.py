1d8de2a533bed0dd59a9364264c899551f830a23
Levi V <levi@redhat.com>
2021-12-10 01:16:16 +0100

    last working version w/ multithread

--- link-checker/multi.py
+++ link-checker/multi.py
@@ -4,7 +4,8 @@ import subprocess
 import re
 import urllib.request
 import urllib.error
-from multiprocessing import Process
+import queue
+from threading import Thread
 
 
 class Regex:
@@ -35,7 +36,7 @@ def get_all_master_adoc_files():
 def build_all_master_adoc_files(master_adocs):
     for master in master_adocs:
         commands = 'asciidoctor --safe -v -n {} > /dev/null 2>&1'.format(master)
-        processes = [subprocess.Popen(commands, shell=True)]
+        process = subprocess.Popen(commands, shell=True)
 
 
 def get_all_master_html_files():
@@ -46,31 +47,70 @@ def get_all_master_html_files():
     return all_master_html_files
 
 
-def load_link(link):
-    req = urllib.request.Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-    req = urllib.request.urlopen(req)
+def check_links(links):
+    for link in links:
+        if not link.startswith('http'):
+            pass
+        else:
+            request = urllib.request.Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+            try:
+                response = urllib.request.urlopen(request)
+            except urllib.error.HTTPError as e:
+                print(bcolors.FAIL + '\tHTTPError: {}'.format(e.code) + ', ' + link + bcolors.ENDC)
+            except urllib.error.URLError as e:
+                print(bcolors.FAIL + '\tURLError: {}'.format(e.reason) + ', ' + link + bcolors.ENDC)
 
-    return req.readall()
+
+def perform_web_requests(addresses, no_workers):
+    class Worker(Thread):
+        def __init__(self, request_queue):
+            Thread.__init__(self)
+            self.queue = request_queue
+            self.results = []
+
+        def run(self):
+            while True:
+                content = self.queue.get()
+                if content == "":
+                    break
+                check_links(content)
+                self.queue.task_done()
+
+    # Create queue and add addresses
+    q = queue.Queue()
+    for url in addresses:
+        q.put(url)
+
+    # Create workers and add tot the queue
+    workers = []
+    for _ in range(no_workers):
+        worker = Worker(q)
+        worker.start()
+        workers.append(worker)
+    # Workers keep working till they receive an empty string
+        for _ in workers:
+            q.put("")
+    # Join workers to wait till they finished
+    for worker in workers:
+        worker.join()
+
+    # Combine results from all workers
+    r = []
+    for worker in workers:
+        r.extend(worker.results)
+    return r
 
 
 def smth(master_htmls):
+    links = []
 
     for master in master_htmls:
-        print(f"\nChecking {master}")
+        print(f"Checking {master}")
         with open(master, 'r') as file:
             original = file.read()
-            links = re.findall(Regex.LINKS_AND_XREFS, original)
-            for link in links:
-                if not link.startswith('http'):
-                    pass
-                else:
-                    req = urllib.request.Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-                    try:
-                        req = urllib.request.urlopen(req)
-                    except urllib.error.HTTPError as e:
-                        print(bcolors.FAIL + '\tHTTPError: {}'.format(e.code) + ', ' + link + bcolors.ENDC)
-                    except urllib.error.URLError as e:
-                        print(bcolors.FAIL + '\tURLError: {}'.format(e.reason) + ', ' + link + bcolors.ENDC)
+            links.append(re.findall(Regex.LINKS_AND_XREFS, original))
+
+    perform_web_requests(links, 20)
 
 
 if __name__ == "__main__":
