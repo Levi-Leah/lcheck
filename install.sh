e774f88c1c2cb3a608a1b263489642a00f09100b
Levi V <levi@redhat.com>
2022-03-01 15:42:49 +0100

    mods the install script

--- install.sh
+++ install.sh
@@ -2,12 +2,18 @@
 
 path_to_script="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
 lcheck=$path_to_script'/lcheck.sh'
+dependencies="linkchecker asciidoctor"
 
-# install dependencies
-sudo yum install -y linkchecker asciidoctor
+for i in $dependencies; do
+    if command -v $i >/dev/null 2>&1 ; then
+        :
+    else
+        sudo yum install $i -y
+    fi
+done
 
-if [ "grep $lcheck ~/.baschrc" ]; then
-    sed -i "\|${lcheck}|d" ~/.bashrc
+if [ "grep lcheck.sh ~/.bashrc" ]; then
+    sed -i "\|lcheck.sh|d" ~/.bashrc
     echo 'alias lcheck="'$lcheck'"' >> ~/.bashrc
     source ~/.bashrc
 else
